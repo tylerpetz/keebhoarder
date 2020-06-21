c805dd0fc83ebc4b808770bdcdc3dbf5d47ab3a6
anthony <anthonys@booj.com>
2020-06-21 11:08:12 -0600

    adds apollo to lamdas

---
+++ packages/graph/src/lambda/datasources/tracker.js
@@ -0,0 +1,18 @@
+const { RESTDataSource } = require('apollo-datasource-rest');
+const tracker = require('delivery-tracker')
+const courier = tracker.courier(tracker.COURIER.USPS.CODE)
+
+class LaunchAPI extends RESTDataSource {
+  constructor() {
+    super();
+  }
+  
+  async getDeliveryStatus(id) {
+    courier.trace({id}, function (err, result) {
+        console.log(result)
+        return result
+    })
+  }
+}
+
+module.exports = LaunchAPI;
