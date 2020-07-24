28ed2bef35861767085a816bb38050a1b2f9443a
Tyler Petz <tylerpetz@gmail.com>
2020-07-24 00:40:46 -0600

    adds readme and welcome msg

---
+++ README.md
@@ -0,0 +1,9 @@
+<a href="https://keebhoarder.com"><img src="https://media0.giphy.com/media/5xaOcLwEvFOizxHVyVy/giphy.gif?cid=ecf05e47rojokoq3za44mysant3739tuiw418pke97on2vmd&rid=giphy.gif" title="Keebhoarder" alt="Keebhoarder"></a>
+
+<!-- [![FVCproductions]https://media0.giphy.com/media/5xaOcLwEvFOizxHVyVy/giphy.gif?cid=ecf05e47rojokoq3za44mysant3739tuiw418pke97on2vmd&rid=giphy.gif)](https://keebhoarder.com) -->
+
+# Keebhoarder
+
+## Welcome keeblord.
+
+If you are here you r weird. Type `yarn` after cloning the repository. Then start coding stuff.
---
+++ sup-keeblord.js
@@ -0,0 +1,19 @@
+const printMessage = require('print-message');
+ 
+printMessage([
+    "welcome keeblord, don't just sit there, start typing",
+    "start doing web stuff type this command: ",
+    "start graphing type this command: "
+], {
+    border: true, // Enable border
+    color: 'green', // Default text color from console
+    borderColor: 'green', // Border color is yellow
+    borderSymbol: '─', // Symbol that uses for border
+    sideSymbol: '│', // Symbol that uses for side separators
+    leftTopSymbol: 'K', // Symbol that uses for left top corner
+    rightTopSymbol: 'H', // Symbol that uses for right top corner
+    marginTop: 1, // Margin before border is begins
+    marginBottom: 1, // Margin after border is ends
+    paddingTop: 1, // Padding after border begins
+    paddingBottom: 1, // Padding before border ends
+});
