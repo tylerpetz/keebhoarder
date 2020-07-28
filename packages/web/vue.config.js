72b3b5772b52ab9b251db02d622e17c076ef9644
Tyler Petz <tylerpetz@gmail.com>
2020-07-28 00:08:57 -0600

    adds vue config

---
+++ packages/web/vue.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // turn off js source maps in production
+  productionSourceMap: false,
+  // split up chunk vendors file: https://medium.com/hackernoon/the-100-correct-way-to-split-your-chunks-with-webpack-f8a9df5b7758
+  configureWebpack: {
+    optimization: {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 40000,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name (module) {
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1]
+              return `npm.${packageName.replace('@', '')}`
+            }
+          }
+        }
+      }
+    }
+  }
+}
