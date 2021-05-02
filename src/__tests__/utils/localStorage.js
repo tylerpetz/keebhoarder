10f5a0be49c72e5a2d2ac0587de2166d59c2fedc
Tyler Petz <tylerpetz@gmail.com>
2021-05-02 11:20:37 -0600

    remove jest for cypress, e2e ftw

--- src/__tests__/utils/localStorage.js
+++
@@ -1,7 +0,0 @@
-export default new class {
-  store = {};
-  setItem = (key, val) => (this.store[key] = val);
-  getItem = key => this.store[key];
-  removeItem = key => { delete this.store[key] };
-  clear = () => (this.store = {});
-}()
