2fe048595990410063911976fe648d89176822b0
Tyler Petz <tylerpetz@gmail.com>
2021-05-10 16:35:00 -0600

    stub out notification area ui component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,7 @@ export default {
 
 <template>
   <footer
-    class="w-full px-3 py-2 flex justify-between"
+    class="w-full px-3 py-2 flex justify-end"
     :class="{ 'bg-theme-bg-d': $store.getters.loggedIn }"
   >
     <p class="text-theme-text text-xs">
