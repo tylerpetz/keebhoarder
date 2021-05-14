2e08ff4e5d88bd8e018c17ac62e9a8e16f0297f4
Tyler Petz <tylerp@booj.com>
2021-05-14 12:41:56 -0600

    remove vuex-persist, l8r

--- src/store/index.js
+++ src/store/index.js
@@ -1,22 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import VuexPersist from 'vuex-persist'
 import auth from './modules/auth'
 import app from './modules/app'
 import list from './modules/list'
 import item from './modules/item'
 
 Vue.use(Vuex)
-
-const vuexLocalStorage = new VuexPersist({
-  key: 'store', // The key to store the state on in the storage provider.
-  storage: window.localStorage, // or window.sessionStorage or localForage
-  modules: ['app']
-  // Function that passes the state and returns the state with only the objects you want to store.
-  // reducer: state => ({}),
-  // Function that passes a mutation and lets you decide if it should update the state in localStorage.
-  // filter: mutation => (true)
-})
 
 const store = new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
@@ -25,8 +14,7 @@ const store = new Vuex.Store({
     app,
     list,
     item
-  },
-  plugins: [vuexLocalStorage.plugin]
+  }
 })
 
 export default store
