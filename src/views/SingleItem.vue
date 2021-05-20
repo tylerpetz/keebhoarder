4b50e4aeb018c690139c7c04a3a6d546da2b21a2
Tyler Petz <tylerp@booj.com>
2021-05-19 19:59:17 -0600

    add some things to single item page

--- src/views/SingleItem.vue
+++ src/views/SingleItem.vue
@@ -31,30 +31,49 @@ export default {
     class="h-full p-3"
   >
     <div class="flex flex-col">
-      <div class="p-6 bg-theme-bg-d mb-4 flex flex-row justify-between items-center rounded shadow">
-        <div>
-          <h3 class="text-xl leading-6 font-medium text-theme-text flex items-center">
-            {{ $store.getters['item/currentItem'].name }}
+      <div class="p-6 bg-theme-bg-d mb-4 flex flex-col rounded shadow">
+        <div class="flex flex-row justify-between items-center">
+          <div>
+            <h3 class="text-xl leading-6 font-medium text-theme-text flex items-center">
+              {{ $store.getters['item/currentItem'].name }}
 
-            <span class="ml-2 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
-              {{ $store.getters['item/currentItem'].public ? 'Public' : 'Private' }}
-            </span>
-            <!-- <span class="ml-2 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
-              {{ $store.getters['item/currentItem'].owned ? '' : 'I Want This' }}
-            </span> -->
-          </h3>
-          <p class="mt-2 max-w-lg text-sm text-theme-text">
-            {{ $store.getters['item/currentItem'].description }}
-          </p>
+              <span class="ml-2 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
+                {{ $store.getters['item/currentItem'].public ? 'Public' : 'Private' }}
+              </span>
+              <span class="ml-2 inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
+                {{ $store.getters['item/currentItem'].owned ? 'Owned' : 'I Want This' }}
+              </span>
+            </h3>
+          </div>
+          <Keycap
+            theme="mod"
+            cap-style="large"
+            type="button"
+            @click.native="$showModal('ItemModal', { props: { item: $store.getters['item/currentItem'] }})"
+          >
+            Edit {{ $store.getters['item/currentItem'].name }}
+          </Keycap>
         </div>
-        <Keycap
-          theme="mod"
-          cap-style="large"
-          type="button"
-          @click.native="$showModal('ItemModal', { props: { item: $store.getters['item/currentItem'] }})"
-        >
-          Edit {{ $store.getters['item/currentItem'].name }}
-        </Keycap>
+        <div class="flex flex-row">
+          <div class="w-1/2">
+            <img
+              v-for="img in $store.getters['item/currentItem'].photos"
+              :key="img"
+              :src="img"
+              :alt="$store.getters['item/currentItem'].name"
+              class="max-h-96"
+            >
+          </div>
+          <div>
+            <p class="mt-2 max-w-lg text-sm text-theme-text">
+              {{ $store.getters['item/currentItem'].description }}
+            </p>
+            <p class="mt-4 max-w-lg text-sm text-theme-text">
+              Maker: {{ $store.getters['item/currentItem'].details.maker }}<br>Model: {{ $store.getters['item/currentItem'].details.model }}<br>
+              Color: {{ $store.getters['item/currentItem'].details.color }}<br>Color: {{ $store.getters['item/currentItem'].qty }}
+            </p>
+          </div>
+        </div>
       </div>
       <pre class="text-theme-text">{{ $store.getters['item/currentItem'] }}</pre>
     </div>
