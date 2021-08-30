967c0ef82f867e8d690d4576f674a970328e066d
Tyler Petz <tylerp@booj.com>
2021-08-30 17:26:06 -0600

    stub out basic photos page

--- pages/photos.vue
+++ pages/photos.vue
@@ -1,15 +1,95 @@
 <script>
 import supabase from '@/utils/supabase'
+import { bytesToSize } from '@/utils/methods'
 
 export default {
-  name: 'NotFound',
+  name: 'Photos',
+  data() {
+    return {
+      photos: [],
+    }
+  },
   async mounted() {
-    const { data } = await supabase.storage.from('photos').list()
-    console.log('stuff', data)
+    const { data: photos } = await supabase.storage.from('photos').list()
+    this.photos = photos.map((photo) => {
+      const { publicURL } = supabase.storage
+        .from('photos')
+        .getPublicUrl(photo.name)
+      return {
+        ...photo,
+        publicURL,
+      }
+    })
+  },
+  methods: {
+    getFileSize(file) {
+      if (file.metadata && file.metadata.size) {
+        return bytesToSize(file.metadata.size)
+      } else {
+        return '0 Byte'
+      }
+    },
   },
 }
 </script>
 
 <template>
-  <div>stuff</div>
+  <section class="mt-8 pb-16" aria-labelledby="gallery-heading">
+    <h2 id="gallery-heading" class="sr-only">Recently viewed</h2>
+    <ul
+      role="list"
+      class="
+        grid grid-cols-2
+        gap-x-4 gap-y-8
+        sm:grid-cols-3 sm:gap-x-6
+        md:grid-cols-4
+        lg:grid-cols-3
+        xl:grid-cols-4 xl:gap-x-8delete
+      "
+    >
+      <li v-for="photo in photos" :key="photo.id" class="relative">
+        <!-- Current: "ring-2 ring-offset-2 ring-indigo-500", Default: "focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500" -->
+        <div
+          class="
+            ring-2 ring-offset-2 ring-indigo-500
+            group
+            block
+            w-full
+            aspect-w-10 aspect-h-7
+            rounded-lg
+            bg-gray-100
+            overflow-hidden
+          "
+        >
+          <!-- Current: "", Default: "group-hover:opacity-75" -->
+          <img
+            :src="photo.publicURL"
+            :alt="photo.name"
+            class="object-cover pointer-events-none"
+          />
+          <button type="button" class="absolute inset-0 focus:outline-none">
+            <span class="sr-only">View details for IMG_4985.HEIC</span>
+          </button>
+        </div>
+        <p
+          class="
+            mt-2
+            block
+            text-sm
+            font-medium
+            text-gray-900
+            truncate
+            pointer-events-none
+          "
+        >
+          {{ photo.name }}
+        </p>
+        <p class="block text-sm font-medium text-gray-500 pointer-events-none">
+          {{ getFileSize(photo) }}
+        </p>
+      </li>
+
+      <!-- More files... -->
+    </ul>
+  </section>
 </template>
