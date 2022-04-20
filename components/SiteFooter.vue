e972dcb5b4f0cc3a86b9b85198ba925c1ec3ac42
Tyler Petz <tylerpetz@gmail.com>
2022-04-20 12:58:38 -0600

    dynamic year

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -9,7 +9,7 @@ export default {
     class="left-0 w-full px-3 py-2 flex justify-between"
     :class="{ 'bg-theme-bg-d': $store.getters.loggedIn }"
   >
-    <p class="text-theme-text text-xs">&copy; 2021 keebhoarder</p>
+    <p class="text-theme-text text-xs">&copy; {{ new Date().getFullYear() }} keebhoarder</p>
     <p class="text-theme-text text-xs pr-1">
       <!-- about - help - donate -->
       <a
