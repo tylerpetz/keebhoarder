4ef035436899b24b4b00c38d04dac95bc0b04e1e
Tyler Petz <tylerpetz@gmail.com>
2022-08-27 22:12:34 -0600

    package updates

--- nuxt.config.js
+++ nuxt.config.js
@@ -30,10 +30,7 @@ export default {
   css: ['@/assets/main.css', '@/assets/themes.css', '@/assets/table.scss'],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    { src: '~/plugins/tables', mode: 'client' },
-    // { src: '~/plugins/supabase' },
-  ],
+  plugins: [{ src: '~/plugins/tables', mode: 'client' }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
@@ -52,8 +49,6 @@ export default {
     '@nuxtjs/composition-api/module',
     // https://google-fonts.nuxtjs.org/setup
     '@nuxtjs/google-fonts',
-    // https://html-validator.nuxtjs.org/
-    // '@nuxtjs/html-validator',
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
--- plugins/supabase.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-export default ({ $config }, inject) => {
-  const supabaseUrl = $config.supabaseUrl
-  const supabaseKey = $config.supabaseKey
-  const client = createClient(supabaseUrl, supabaseKey)
-  inject('supabase', client)
-}
