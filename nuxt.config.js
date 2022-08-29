ff48c8ab9749dd4ee7e7731b28afbd1e52ddb8f5
Tyler Petz <tylerpetz@gmail.com>
2022-08-29 15:31:02 -0600

    update packages

--- nuxt.config.js
+++ nuxt.config.js
@@ -73,13 +73,7 @@ export default {
   ],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-    loaders: {
-      vue: {
-        compiler: require('vue-template-babel-compiler'),
-      },
-    },
-  },
+  build: {},
   serverMiddleware: ['~/prisma/index.ts'],
 
   googleFonts: {
